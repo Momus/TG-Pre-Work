1a42689b6408481b04fc0c71cda3f71141278301
Dmitri <dmitri@momus.net>
2018-07-21 17:27:35 -0600

    JS Variables

---
+++ Code-Academy-Exercises/JavaScript/variables.js
@@ -0,0 +1,53 @@
+// This line of code sets the variable location to the string New York City
+const location = 'New York City';
+
+// This line of code sets the variable latitude to the number 40.7
+let latitude = 40.7;
+
+// This line of code sets the variable inNorthernHemisphere to true
+let inNorthernHemisphere = true;
+
+console.log(location);
+console.log(latitude);
+console.log(inNorthernHemisphere);
+
+
+const entree = 'Enchiladas';
+const price = 12;
+console.log(entree);
+console.log(price);
+entree = 'Tacos'; //TypeError: Assignment to constant variable.
+
+
+let changeMe = true;
+changeMe = false;
+console.log(changeMe)
+
+
+let notDefined;
+console.log(notDefined);
+let valueless;
+console.log(valueless);
+
+
+let molecule = 16;
+let particle = 18;
+let assay = 3;
+// Add and assign to molecule below
+molecule +=16
+// Multiply and assign to particle below
+particle *= 6.02
+// Increment assay by 1
+assay++
+
+
+let favoriteAnimal = "whale";
+console.log("My favorite animal:" + favoriteAnimal);
+
+
+let myName = "Dmitri";
+let myCity = "Darwin";
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+
+
