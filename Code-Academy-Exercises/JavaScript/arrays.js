111f1805cd9661ec49a72d40455180479196fbd5
Dmitri <dmitri@momus.net>
2018-07-22 09:12:00 -0600

    JS Arrays

---
+++ Code-Academy-Exercises/JavaScript/arrays.js
@@ -0,0 +1,45 @@
+let newYearsResolutions = ["don't", "make", "any"]
+console.log(newYearsResolutions);
+
+
+let newYearsResolutions = ["don't", "make", "any"]
+console.log(newYearsResolutions);
+let listItem = newYearsResolutions[0]
+console.log(listItem);
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
+
+
+newYearsResolutions[1] = 'Learn a new language';
+
+
+console.log(newYearsResolutions.length);
+
+
+newYearsResolutions.push('please', 'just don\'t');
+newYearsResolutions.pop();
+
+
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown
+rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList.shift();
+console.log(groceryList);
+groceryList.unshift('popcorn');
+console.log(groceryList);
+console.log(groceryList.slice(1,4));
+console.log(groceryList);
+
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+condiments.push('salsa');
+console.log(condiments);
+condiments = ['salsa'];
+console.log(condiments);
+utensils.pop();
+console.log(utensils);
+utensils = ['leatherman'];
+
+
+
+
